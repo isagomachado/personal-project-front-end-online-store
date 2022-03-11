a815c16bd5f4d48c0323a3b524934b430269c1c2
Eduardo <dudu-system@hotmail.com>
2022-03-11 15:02:50 -0300

    requisito 02

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route path="/" component={ Home } />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Home extends React.Component{
+  render () {
+    return(
+      <>
+        <div>
+          <input 
+            type="text"
+          />
+        </div>
+        <div data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+      </>
+    )
+  }
+}
+
+export default Home;
